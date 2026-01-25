a39dac47c7685cef89823b164e6b0cd55e62429a
Aksh Naik <akshnaik1907@gmail.com>
2026-01-25 12:12:24 +0530

    Ready for Deployement .

--- server.js
+++ server.js
@@ -12,7 +12,8 @@ app.use(express.json());
 app.use("/api/test", protectedRoutes);
 app.use("/api/progress", progressRoutes);
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
+
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
